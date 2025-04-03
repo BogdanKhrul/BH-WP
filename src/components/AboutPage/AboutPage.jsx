2d5ae682bd407128eb095f549d5a41db4b08ef1d
BogdanKhrul <bobb52654@gmail.com>
2025-04-03 17:34:13 +0200

    03.04.2025

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -3,7 +3,15 @@ import css from './AboutPage.module.scss';
 const AboutPage = () => {
     return (
         <div className={css.screen}>
-            <div></div>
+            <div className={css.block}>
+                <div className={css.navBar}>
+                    <div className={css.child}>About Me</div>
+                    <div className={css.child}>Skills</div>
+                    <div className={css.child}>Contacts</div>
+                    <div className={css.child}>Web Sites</div>
+                    <div className={css.child}>Others</div>
+                </div>
+            </div>
         </div>
     );
 };
