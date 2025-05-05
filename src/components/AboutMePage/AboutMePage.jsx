eeb619d95cc4a53fe291cbc10406d752d377bb4a
BogdanKhrul <bobb52654@gmail.com>
2025-05-05 15:09:05 +0200

    05.05.2025

--- src/components/AboutMePage/AboutMePage.jsx
+++ src/components/AboutMePage/AboutMePage.jsx
@@ -1,25 +1,42 @@
 import css from './AboutMePage.module.scss';
+import { TbBrandReact } from 'react-icons/tb';
+import { GrProjects } from 'react-icons/gr';
 
 const AboutMePage = () => {
     return (
         <div className={css.screen}>
-            <h2>About Me</h2>
-            <h3>Transforming ideas into projects</h3>
-            <div className={css.blockOne}>
-                <p>Hello, I'm Bohdan Khrul</p>
-                <p>
-                    I am a Full-Stack Web Developer who has been developing for two years and I turn ideas into working
-                    solutions. I work on both Frontend and Backend, creating simple and effective applications. My
-                    approach is quality code, attention to detail and striving for perfection in every project. I don’t
-                    just write code, I solve problems. If you are looking for a developer who is not afraid of new
-                    challenges - I am your man.
-                </p>
-                <div>
-                    <a href="">Download CV</a>
-                    <a href="">My Projects</a>
+            <div className={css.decBox}>
+                <h2>About Me</h2>
+                <h3 className={css.decTxt}>
+                    <TbBrandReact className={css.reactIcon} />
+                    Transforming ideas into projects
+                    <TbBrandReact className={css.reactIcon} />
+                </h3>
+            </div>
+            <div className={css.mainBlock}>
+                <div className={css.blockTwo}>
+                    <div className={css.wrapper}></div>
+                </div>
+                <div className={css.blockOne}>
+                    <p className={css.nameTxt}>Hello, I'm Bohdan Khrul</p>
+                    <p className={css.aboutTxt}>
+                        I am a <span>Full-Stack Web Developer</span> who has been developing for two years and I turn
+                        ideas into working solutions. I work on both <span>Frontend</span> and <span>Backend</span>,
+                        creating simple and effective applications. My approach is quality code, attention to detail and
+                        striving for perfection in every project. I don’t just write code, I solve problems. If you are
+                        looking for a developer who is not afraid of new challenges - I am your man.
+                    </p>
+                    <div className={css.btnBox}>
+                        <a className={css.btnOne} href="">
+                            Download CV
+                        </a>
+                        <a className={css.btnTwo} href="">
+                            <GrProjects className={css.projectsIcon} />
+                            My Projects
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div className={css.blockTwo}></div>
         </div>
     );
 };
---
+++ src/components/media/svg-icons/Readme.txt
@@ -0,0 +1,3 @@
+I know that it would be more reasonable 
+to make all the svg pictures through a sprite,
+but I decided to do it this way :D
